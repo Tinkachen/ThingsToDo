b5b5f5f7af6f49e0a9148b5ab06e57b55e80aeb4
Tinkachen <catharina.herchert@trevisto.de>
2018-07-02 22:12:38 +0200

    added default values
    for creating a new todo list and todo task

--- TTD/Factories/ViewModelFactory.swift
+++ TTD/Factories/ViewModelFactory.swift
@@ -8,6 +8,20 @@
 
 import Foundation
 
+/// Private constants
+private enum Constants {
+    
+    /// Localized Strings for view models
+    enum Strings {
+        
+        /// Title for a new todo list
+        static let newTodoList = NSLocalizedString("NTLVC_title", comment: "Title for a new todo list")
+        
+        /// Title for a new todo task
+        static let newTodoTask = NSLocalizedString("NTTVC_title", comment: "Title for a new todo task")
+    }
+}
+
 /// Foctory for creating view models
 struct ViewModelFactory {
     
@@ -15,14 +29,21 @@ struct ViewModelFactory {
     ///
     /// - Returns: The instance
     static func makeTodoListViewModel () -> TodoListViewModel {
-        return TodoListViewModel ()
+        return TodoListViewModel(id: UUID().uuidString,
+                                 gradient: Gradient.rdmGradient(),
+                                 icon: Icon.rdmIcon(),
+                                 title: Constants.Strings.newTodoList,
+                                 passcode: nil,
+                                 tasks: nil)
     }
     
     /// Makes an instance of 'TodoTaskViewModel'
     ///
     /// - Returns: The instance
     static func makeTodoTaskVieModel () -> TodoTaskViewModel {
-        return TodoTaskViewModel()
+        var viewModel = TodoTaskViewModel()
+        viewModel.taskDescription = Constants.Strings.newTodoTask
+        return viewModel
     }
     
     /// Makes an instance of the 'PasscodeViewModel'

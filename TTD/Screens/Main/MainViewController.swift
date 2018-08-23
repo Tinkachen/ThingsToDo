39a988be80bd06185257bfcde54e0e9366cc86f2
Tinkachen <catharina-herchert@gmx.de>
2018-08-23 10:59:02 +0200

    master: reworked gradient handling / removed unused

--- TTD/Screens/Main/MainViewController.swift
+++ TTD/Screens/Main/MainViewController.swift
@@ -19,6 +19,12 @@ private enum Constants {
         
         /// The localized text to welcome the user
         static let welcomeUserKey = "MVC_welcome_user"
+        
+        /// The localized text to delete a cell
+        static let alertDeleteKey = "MVC_alert_delete"
+        
+        /// The localized text to cancel the cell delete
+        static let alertCancelKey = "MVC_alert_cancel"
     }
 }
 
@@ -75,63 +81,9 @@ class MainViewController: UIViewController {
         
     }
     
-    /// <#Description#>
+    /// Requests the list view models from the local storage
     private func getListViewModels () {
         todoListViewModels = TodoListService.getListViewModels()
-    }
-    
-    /// <#Description#>
-    ///
-    /// - Parameters:
-    ///   - viewModel1: <#viewModel1 description#>
-    ///   - viewModel2: <#viewModel2 description#>
-    fileprivate func fadeFromViewModel(_ viewModel1: TodoListViewModel, ToViewModel viewModel2: TodoListViewModel, withPercentage percentage: CGFloat) {
-        
-//        let from1 = UIColor(cgColor: viewModel1.getGradient()[0])
-//        var fRed1: CGFloat = 0
-//        var fGreen1: CGFloat = 0
-//        var fBlue1: CGFloat = 0
-//        var fAlpha1: CGFloat = 0
-//        from1.getRed(&fRed1, green: &fGreen1, blue: &fBlue1, alpha: &fAlpha1)
-//
-//        let to1 = UIColor(cgColor: viewModel1.getGradient()[0])
-//        var tRed1: CGFloat = 0
-//        var tGreen1: CGFloat = 0
-//        var tBlue1: CGFloat = 0
-//        var tAlpha1: CGFloat = 0
-//        to1.getRed(&tRed1, green: &tGreen1, blue: &tBlue1, alpha: &tAlpha1)
-//
-//        let red1 = (tRed1 - fRed1) * percentage + fRed1
-//        let green1 = (tGreen1 - fGreen1) * percentage + fGreen1
-//        let blue1 = (tBlue1 - fBlue1) * percentage + fBlue1
-//        let alpha1 = (tAlpha1 - fAlpha1) * percentage + fAlpha1
-//
-//        let newTopGradient = UIColor(red: red1, green: green1, blue: blue1, alpha: alpha1)
-//
-//        let from2 = UIColor(cgColor: viewModel1.getGradient()[1])
-//        var fRed2: CGFloat = 0
-//        var fGreen2: CGFloat = 0
-//        var fBlue2: CGFloat = 0
-//        var fAlpha2: CGFloat = 0
-//        from2.getRed(&fRed2, green: &fGreen2, blue: &fBlue2, alpha: &fAlpha2)
-//
-//        let to2 = UIColor(cgColor: viewModel1.getGradient()[1])
-//        var tRed2: CGFloat = 0
-//        var tGreen2: CGFloat = 0
-//        var tBlue2: CGFloat = 0
-//        var tAlpha2: CGFloat = 0
-//        to2.getRed(&tRed2, green: &tGreen2, blue: &tBlue2, alpha: &tAlpha2)
-//
-//        let red2 = (tRed2 - fRed2) * percentage + fRed2
-//        let green2 = (tGreen2 - fGreen2) * percentage + fGreen2
-//        let blue2 = (tBlue2 - fBlue2) * percentage + fBlue2
-//        let alpha2 = (tAlpha2 - fAlpha2) * percentage + fAlpha2
-//
-//        let newBottomGradient = UIColor(red: red2, green: green2, blue: blue2, alpha: alpha2)
-//
-//        self.view.removeGradientLayer()
-//        self.view.applyGradient(colors: [newTopGradient.cgColor, newBottomGradient.cgColor])
-        
     }
     
     // MARK: - Buttons Actions
@@ -146,11 +98,14 @@ class MainViewController: UIViewController {
     
     fileprivate func deleteCell (atIndexPath indexPath: IndexPath) {
         let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        alertController.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { (alertAction) in
+        alertController.addAction(UIAlertAction(title: Constants.Strings.alertDeleteKey, style: .destructive, handler: { (alertAction) in
             let vm = self.todoListViewModels[indexPath.row]
             vm.deleteListViewModel()
             self.todoListViewModels.remove(at: indexPath.row)
             self.collectionView.reloadData()
+        }))
+        alertController.addAction(UIAlertAction(title: Constants.Strings.alertCancelKey, style: .cancel, handler: { (alertAction) in
+            alertController.dismiss(animated: true, completion: nil)
         }))
         
         self.present(alertController, animated: true, completion: nil)
@@ -220,28 +175,6 @@ extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSour
         return Int(round(proportionalOffset))
     }
     
-    /*func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let page = indexOfMajorCell()
-        var oldPage: Int = 0
-        let currentPageX = CGFloat(page) * scrollView.frame.width
-        
-        if (scrollView.contentOffset.x >= currentPageX + scrollView.frame.width) { // right
-            oldPage = page + 1
-        } else { // left
-            oldPage = page - 1
-        }
-        
-        if page > todoListViewModels.count || page < 0 { return }
-        if oldPage > todoListViewModels.count || oldPage < 0 { return }
-        
-        let maxHorizontalOffset = scrollView.contentSize.width - scrollView.frame.width
-        let currentHorizontalOffset = scrollView.contentOffset.x
-        
-        let percentageHorizontalOffset = currentHorizontalOffset / self.view.frame.width - CGFloat(page)
-        
-        fadeFromViewModel(todoListViewModels[oldPage], ToViewModel: todoListViewModels[page], withPercentage: percentageHorizontalOffset)
-    }*/
-    
     func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
         indexOfCellBeforeDragging = indexOfMajorCell()
     }
@@ -277,9 +210,9 @@ extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSour
             let indexPath = IndexPath(row: indexOfMajorCell, section: 0)
             collectionView.collectionViewLayout.collectionView!.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
         }
+        
         let from = todoListViewModels[indexOfCellBeforeDragging]
         let to = todoListViewModels[indexOfMajorCell]
-        self.view.removeGradientLayer()
         self.view.transitToGradient(from: from.getGradient(), to: to.getGradient())
     }
 }

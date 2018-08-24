5ca7b2e4c5a36855dd63d1c578526d4824c57c6d
Tinkachen <catharina-herchert@gmx.de>
2018-08-24 13:46:09 +0200

    master: update progress bar when deleting cells

--- TTD/Screens/TodoList/TodoListViewController.swift
+++ TTD/Screens/TodoList/TodoListViewController.swift
@@ -352,8 +352,8 @@ extension TodoListViewController: UITableViewDelegate, UITableViewDataSource {
         
         cell.applyData(taskViewModel, deleteCallback: {
             self.viewModel.deleteTaskViewModel(atIndex: indexPath.row)
-            
             tableView.deleteRows(at: [indexPath], with: .fade)
+            self.applyProgressBarLayout()
         }, doneStateCallback: {
             self.viewModel.updateTasks()
             self.applyProgressBarLayout()

cb99051cfd1af9d238f6ceb0893e4587b4a77011
Tinkachen <catharina-herchert@gmx.de>
2018-12-19 08:35:21 +0100

    master: added global environment class with print fkt

---
+++ TTD/Environment.swift
@@ -0,0 +1,19 @@
+//
+//  Environment.swift
+//  TTD
+//
+//  Created by Catharina Herchert on 19.12.18.
+//  Copyright © 2018 Catharina Herchert. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/// Prints the passed string to the debugging console with class informations
+///
+/// - Parameters:
+///   - txt: The string that will be printed to console
+///   - sender: The sender of the call
+public func print (_ txt: String, _ sender: AnyClass) {
+    print("[\(sender)] -> \(txt)]")
+}
